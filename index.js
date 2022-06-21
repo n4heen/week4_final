9eb7835b20c718a496c18a5b97c3bebe8b0acadf
n4heen <101859246+n4heen@users.noreply.github.com>
2022-06-21 21:45:31 +1000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+/* Here is your key: 6954bedd
+
+Send all data requests to:
+http://www.omdbapi.com/?apikey=[yourkey]&
+
+Poster API requests:
+http://img.omdbapi.com/?apikey=[yourkey]&
+
+To see fast and furious movies
+https://www.omdbapi.com/?apikey=6954bedd&s=fast
+
+Please append it to all of your API requests,*/
