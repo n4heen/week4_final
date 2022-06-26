be4dde21bfdc9c1843e63c6dbdd3ef87c8668d79
Naheen Habib <naheenhabib@outlook.com>
2022-06-26 20:03:14 +1000

    new commit

--- index.js
+++ index.js
@@ -15,10 +15,20 @@ Please append it to all of your API requests*/
 
 const filmListEl = document.querySelector(".film-list")
 async function main() {
-    const film = await fetch('http://www.omdbapi.com/?apikey=6954bedd&')
+    const film = await fetch('https://www.omdbapi.com/?i=tt3896198&apikey=6954bedd')
     const filmData = await film.json()
-    console.log(filmData)
+   console.log(filmData)
 }
 main()
 
-
+function filmHTML(film) {
+    return `<div class="film-card">
+    <div class="film-card__container">
+        <h3>egg title</h3>
+            <p><b>Year:</b>0000</p>
+            <p><b>Type:</b> film</p>
+            <p><b>imdbID:</b>ID</p>
+            <p><b>Movie Poster</b>Poster</p>
+    </div>
+</div>`
+}
