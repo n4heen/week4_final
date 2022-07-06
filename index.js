5e5b27e18f1cdd3e2ec19711affffc7cb032a157
Naheen Habib <naheenhabib@outlook.com>
2022-07-06 20:59:11 +1000

    new commit

--- index.js
+++ index.js
@@ -13,41 +13,41 @@ https://www.omdbapi.com/?i=tt3896198&apikey=6954beddy
 
 Please append it to all of your API requests*/
 
-/*
-Hi n4heen,
 
-Your request for an API key has been approved. You can start using this key immediately.
-
-API Key: b98d570916d83c3feaa4a3301fdf2261
-
-Here's an example API request:
-
-https://api.themoviedb.org/3/movie/550?api_key=b98d570916d83c3feaa4a3301fdf2261
-Useful Links
-
-Documentation: https://developers.themoviedb.org/3/getting-started/introduction
-Support forum: https://www.themoviedb.org/talk/category/5047958519c29526b50017d6
-Wrappers & libraries: https://www.themoviedb.org/documentation/api/wrappers-libraries
-Service status: https://status.themoviedb.org
-If you have any questions, feel free to create a new discussion in our support forum.
---
-The Movie Database Team
-https://www.themoviedb.org
-*/
+// const filmListEl = document.querySelector('.film-list')
+// async function filmApi() {
+//   const films = await fetch('https://www.omdbapi.com/?apikey=6954bedd&s=fast')
+//   const filmData = await films.json()
+//   filmListEl.innerHTML = filmData.Search.map((film) => { return
+//     filmHTML(film)
+//   }).join("")
+//   console.log(filmData.Search)
+// }
+// filmApi()
 
 
-const filmListEl = document.querySelector('.film-list')
+// function filmHTML(film) {
+//   return `<div class="film-card">
+//     <div class="film-card__container">
+//         <h3>film title</h3>
+//             <p><b>Year:</b>0000</p>
+//             <p><b>Type:</b> film</p>
+//             <p><b>imdbID:</b>ID</p>
+//             <p><b>Movie Poster</b>Poster</p>
+//     </div>
+// </div>`
+// }
+
+const filmListEl = document.querySelector(".film-list");
 async function filmApi() {
-  const films = await fetch('https://www.omdbapi.com/?apikey=6954bedd&s=fast')
-  const filmData = await films.json()
-  console.log(filmData.Search.map((film) => {
-    filmHTML(film)
-  }).join(""))
+  const films = await fetch("https://www.omdbapi.com/?apikey=6954bedd&s=fast");
+  const filmData = await films.json();
+  filmListEl.innerHTML = filmData.Search.map(() => filmHTML()).join("");
+  console.log(filmData.Search);
 }
-filmApi()
+filmApi();
 
-
-function filmHTML(film) {
+function filmHTML() {
   return `<div class="film-card">
     <div class="film-card__container">
         <h3>film title</h3>
@@ -56,6 +56,5 @@ function filmHTML(film) {
             <p><b>imdbID:</b>ID</p>
             <p><b>Movie Poster</b>Poster</p>
     </div>
-</div>`
+</div>`;
 }
-
