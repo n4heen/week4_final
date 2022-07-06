e1ec1210415a7b75a76ffae771c1e0313e4cbff2
Naheen Habib <naheenhabib@outlook.com>
2022-07-06 18:13:53 +1000

    new commit

--- index.js
+++ index.js
@@ -9,16 +9,11 @@ http://img.omdbapi.com/?apikey=[yourkey]&
 To see fast and furious movies
 https://www.omdbapi.com/?apikey=6954bedd&s=fast
 
+https://www.omdbapi.com/?i=tt3896198&apikey=6954beddy
+
 Please append it to all of your API requests*/
 
 /*
-This is the TMDB API INFO
-API Key (v3 auth) - b98d570916d83c3feaa4a3301fdf2261
-
-Example API Request - https://api.themoviedb.org/3/movie/550?api_key=b98d570916d83c3feaa4a3301fdf2261
-
-API Read Access Token (v4 auth) - eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiOThkNTcwOTE2ZDgzYzNmZWFhNGEzMzAxZmRmMjI2MSIsInN1YiI6IjYyYzUzYmVkYzc0ZWJhMDA1NzJkMjYyOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.8NLvIerD44c4dYof_YLMVrFCKVy5QHD8okBUteeNstA
-
 Hi n4heen,
 
 Your request for an API key has been approved. You can start using this key immediately.
@@ -44,7 +39,7 @@ https://www.themoviedb.org
 
 const filmListEl = document.querySelector('.film-list')
 async function filmApi() {
-    const films = await fetch('https://api.themoviedb.org/3/movie/550?api_key=b98d570916d83c3feaa4a3301fdf2261')
+    const films = await fetch('https://www.omdbapi.com/?apikey=6954bedd&')
     const filmData = await films.json()
     filmListEl.innerHTML= filmData.map((film)=>{
       filmHTML(film)
